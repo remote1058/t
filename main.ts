a2cb9141b48f702e8f69a3285257f1a91b40fb15
remote1058 <120849740+remote1058@users.noreply.github.com>
2023-08-08 13:52:47 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,19 +1,20 @@
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    if (run == "F") {
-        run = "S"
-    } else {
-        run = "F"
-    }
-    radio.sendString(run)
-    basic.showString(run)
+    radio.sendString("S")
+    basic.showString("S")
 })
 input.onButtonPressed(Button.A, function () {
     radio.sendString("L")
     basic.showString("L")
 })
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("F")
+    basic.showString("F")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
 input.onButtonPressed(Button.B, function () {
     radio.sendString("R")
     basic.showString("R")
 })
-let run = ""
 radio.setGroup(1)
